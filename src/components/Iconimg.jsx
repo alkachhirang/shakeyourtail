19b8ec48cfa92e7a9afdb72519b56436cf9eebe5
alkachhirang <alkachhirang3@gmail.com>
2023-11-16 12:35:37 +0530

    preloader-color

--- src/components/Iconimg.jsx
+++ src/components/Iconimg.jsx
@@ -465,7 +465,7 @@ export const Lightorangeflower = () => {
 };
 export const Preloader = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center min-vh-100 position-fixed end-0 start-0">
+        <div className="d-flex justify-content-center align-items-center min-vh-100 position-fixed end-0 start-0 bg-black">
             <svg className="mw_100 w-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><radialGradient id="a10" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)"><stop offset="0" stopColor="#FF156D"></stop><stop offset=".3" stopColor="#FF156D" stopOpacity=".9"></stop><stop offset=".6" stopColor="#FF156D" stopOpacity=".6"></stop><stop offset=".8" stopColor="#FF156D" stopOpacity=".3"></stop><stop offset="1" stopColor="#FF156D" stopOpacity="0"></stop></radialGradient><circle transform-origin="center" fill="none" stroke="url(#a10)" strokeWidth="15" strokeLinecap="round" strokeDasharray="200 1000" strokeDashoffset="0" cx="100" cy="100" r="70"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></circle><circle transform-origin="center" fill="none" opacity=".2" stroke="#FF156D" strokeWidth="15" strokeLinecap="round" cx="100" cy="100" r="70"></circle></svg>
         </div>
     );
